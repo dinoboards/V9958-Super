58973fc13b66cd832688bcdaa7aa66eb28ed2205
Dean Netherton <dinoboards@netherton.org>
2024-03-02 18:14:59 +1100

    audio_sample_packet/packet_picker: minor formatting adjustment and removed a couple of warnings

--- src/hdmi/audio_sample_packet.sv
+++ src/hdmi/audio_sample_packet.sv
@@ -59,51 +59,52 @@ module audio_sample_packet
     output logic [55:0] sub [3:0]
 );
 
-// Left/right channel for stereo audio
-logic [3:0] CHANNEL_LEFT = 4'd1;
-logic [3:0] CHANNEL_RIGHT = 4'd2;
+  // Left/right channel for stereo audio
+  localparam [3:0] CHANNEL_LEFT = 4'd1;
+  localparam [3:0] CHANNEL_RIGHT = 4'd2;
 
-localparam bit [7:0] CHANNEL_STATUS_LENGTH = 8'd192;
-// See IEC 60958-1 5.1, Table 2
-logic [192-1:0] channel_status_left;
-assign channel_status_left = {152'd0, ORIGINAL_SAMPLING_FREQUENCY, WORD_LENGTH, 2'b00, CLOCK_ACCURACY, SAMPLING_FREQUENCY, CHANNEL_LEFT, SOURCE_NUMBER, CATEGORY_CODE, MODE, PRE_EMPHASIS, COPYRIGHT_NOT_ASSERTED, SAMPLE_WORD_TYPE, GRADE};
-logic [CHANNEL_STATUS_LENGTH-1:0] channel_status_right;
-assign channel_status_right = {152'd0, ORIGINAL_SAMPLING_FREQUENCY, WORD_LENGTH, 2'b00, CLOCK_ACCURACY, SAMPLING_FREQUENCY, CHANNEL_RIGHT, SOURCE_NUMBER, CATEGORY_CODE, MODE, PRE_EMPHASIS, COPYRIGHT_NOT_ASSERTED, SAMPLE_WORD_TYPE, GRADE};
+  localparam bit [7:0] CHANNEL_STATUS_LENGTH = 8'd192;
+  // See IEC 60958-1 5.1, Table 2
+  logic [192-1:0] channel_status_left;
+  assign channel_status_left = {152'd0, ORIGINAL_SAMPLING_FREQUENCY, WORD_LENGTH, 2'b00, CLOCK_ACCURACY, SAMPLING_FREQUENCY, CHANNEL_LEFT, SOURCE_NUMBER, CATEGORY_CODE, MODE, PRE_EMPHASIS, COPYRIGHT_NOT_ASSERTED, SAMPLE_WORD_TYPE, GRADE};
+  logic [CHANNEL_STATUS_LENGTH-1:0] channel_status_right;
+  assign channel_status_right = {152'd0, ORIGINAL_SAMPLING_FREQUENCY, WORD_LENGTH, 2'b00, CLOCK_ACCURACY, SAMPLING_FREQUENCY, CHANNEL_RIGHT, SOURCE_NUMBER, CATEGORY_CODE, MODE, PRE_EMPHASIS, COPYRIGHT_NOT_ASSERTED, SAMPLE_WORD_TYPE, GRADE};
 
 
-// See HDMI 1.4a Table 5-12: Audio Sample Packet Header.
-assign header[19:12] = {4'b0000, {3'b000, LAYOUT}};
-assign header[7:0] = 8'd2;
-logic [1:0] parity_bit [3:0];
-logic [7:0] aligned_frame_counter [3:0];
-genvar i;
-generate
+  // See HDMI 1.4a Table 5-12: Audio Sample Packet Header.
+  assign header[19:12] = {4'b0000, {3'b000, LAYOUT}};
+  assign header[7:0] = 8'd2;
+  logic [1:0] parity_bit [3:0];
+  logic [7:0] aligned_frame_counter [3:0];
+  genvar i;
+  generate
     for (i = 0; i < 4; i++)
     begin: sample_based_assign
-        always_comb
-        begin
-            if (8'(frame_counter + i) >= CHANNEL_STATUS_LENGTH)
-                aligned_frame_counter[i] = 8'(frame_counter + i - CHANNEL_STATUS_LENGTH);
-            else
-                aligned_frame_counter[i] = 8'(frame_counter + i);
-        end
-        assign header[23 - (3-i)] = aligned_frame_counter[i] == 8'd0 && audio_sample_word_present[i];
-        assign header[11 - (3-i)] = audio_sample_word_present[i];
-        assign parity_bit[i][0] = ^{channel_status_left[aligned_frame_counter[i]], user_data_bit[i][0], valid_bit[i][0], audio_sample_word[i][0]};
-        assign parity_bit[i][1] = ^{channel_status_right[aligned_frame_counter[i]], user_data_bit[i][1], valid_bit[i][1], audio_sample_word[i][1]};
-        // See HDMI 1.4a Table 5-13: Audio Sample Subpacket.
-        always_comb
-        begin
-            if (audio_sample_word_present[i])
-                sub[i] = {{parity_bit[i][1], channel_status_right[aligned_frame_counter[i]], user_data_bit[i][1], valid_bit[i][1], parity_bit[i][0], channel_status_left[aligned_frame_counter[i]], user_data_bit[i][0], valid_bit[i][0]}, audio_sample_word[i][1], audio_sample_word[i][0]};
-            else
-            `ifdef MODEL_TECH
-                sub[i] = 56'd0;
-            `else
-                sub[i] = 56'dx;
-            `endif
-        end
+      always_comb
+      begin
+        if (8'(frame_counter + i) >= CHANNEL_STATUS_LENGTH)
+          aligned_frame_counter[i] = 8'(frame_counter + i - CHANNEL_STATUS_LENGTH);
+        else
+          aligned_frame_counter[i] = 8'(frame_counter + i);
+      end
+      assign header[23 - (3-i)] = aligned_frame_counter[i] == 8'd0 && audio_sample_word_present[i];
+      assign header[11 - (3-i)] = audio_sample_word_present[i];
+      assign parity_bit[i][0] = ^{channel_status_left[aligned_frame_counter[i]], user_data_bit[i][0], valid_bit[i][0], audio_sample_word[i][0]};
+      assign parity_bit[i][1] = ^{channel_status_right[aligned_frame_counter[i]], user_data_bit[i][1], valid_bit[i][1], audio_sample_word[i][1]};
+      // See HDMI 1.4a Table 5-13: Audio Sample Subpacket.
+
+      always_comb
+      begin
+        if (audio_sample_word_present[i])
+          sub[i] = {{parity_bit[i][1], channel_status_right[aligned_frame_counter[i]], user_data_bit[i][1], valid_bit[i][1], parity_bit[i][0], channel_status_left[aligned_frame_counter[i]], user_data_bit[i][0], valid_bit[i][0]}, audio_sample_word[i][1], audio_sample_word[i][0]};
+        else
+        `ifdef MODEL_TECH
+          sub[i] = 56'd0;
+        `else
+          sub[i] = 56'dx;
+        `endif
+      end
     end
-endgenerate
+  endgenerate
 
 endmodule
--- src/hdmi/packet_picker.sv
+++ src/hdmi/packet_picker.sv
@@ -1,8 +1,7 @@
 // Implementation of HDMI packet choice logic.
 // By Sameer Puri https://github.com/sameer
 
-module packet_picker
-#(
+module packet_picker #(
     parameter int VIDEO_ID_CODE = 4,
     parameter real VIDEO_RATE = 0,
     parameter bit IT_CONTENT = 1'b0,
@@ -26,6 +25,8 @@ module packet_picker
 
 // Connect the current packet type's data to the output.
 logic [7:0] packet_type = 8'd0;
+
+//TODO: investigate, do we need headers and sub to be such large arrays
 logic [23:0] headers [255:0];
 logic [55:0] subs [255:0] [3:0];
 assign header = headers[packet_type];
@@ -37,7 +38,8 @@ assign sub[3] = subs[packet_type][3];
 // NULL packet
 // "An HDMI Sink shall ignore bytes HB1 and HB2 of the Null Packet Header and all bytes of the Null Packet Body."
 `ifdef MODEL_TECH
-assign headers[0] = {8'd0, 8'd0, 8'd0}; assign subs[0] = '{56'd0, 56'd0, 56'd0, 56'd0};
+assign headers[0] = {8'd0, 8'd0, 8'd0};
+assign subs[0] = '{56'd0, 56'd0, 56'd0, 56'd0};
 `else
 assign headers[0] = {8'dX, 8'dX, 8'd0};
 assign subs[0][0] = 56'dX;
@@ -48,7 +50,16 @@ assign subs[0][3] = 56'dX;
 
 // Audio Clock Regeneration Packet
 logic clk_audio_counter_wrap;
-audio_clock_regeneration_packet #(.VIDEO_RATE(VIDEO_RATE), .AUDIO_RATE(AUDIO_RATE)) audio_clock_regeneration_packet (.clk_pixel(clk_pixel), .clk_audio(clk_audio), .clk_audio_counter_wrap(clk_audio_counter_wrap), .header(headers[1]), .sub(subs[1]));
+audio_clock_regeneration_packet #(
+  .VIDEO_RATE(VIDEO_RATE),
+  .AUDIO_RATE(AUDIO_RATE)
+  ) audio_clock_regeneration_packet (
+    .clk_pixel(clk_pixel),
+    .clk_audio(clk_audio),
+    .clk_audio_counter_wrap(clk_audio_counter_wrap),
+    .header(headers[1]),
+    .sub(subs[1])
+    );
 
 // Audio Sample packet
 localparam bit [3:0] SAMPLING_FREQUENCY = AUDIO_RATE == 32000 ? 4'b0011
@@ -128,19 +139,41 @@ begin
             frame_counter = frame_counter - 8'd192;
     end
 end
-audio_sample_packet #(.SAMPLING_FREQUENCY(SAMPLING_FREQUENCY), .WORD_LENGTH({{WORD_LENGTH[0], WORD_LENGTH[1], WORD_LENGTH[2]}, WORD_LENGTH_LIMIT})) audio_sample_packet (.frame_counter(frame_counter), .valid_bit('{2'b00, 2'b00, 2'b00, 2'b00}), .user_data_bit('{2'b00, 2'b00, 2'b00, 2'b00}), .audio_sample_word(audio_sample_word_packet), .audio_sample_word_present(audio_sample_word_present_packet), .header(headers[2]), .sub(subs[2]));
-
+audio_sample_packet #(
+    .SAMPLING_FREQUENCY(SAMPLING_FREQUENCY),
+    .WORD_LENGTH({{WORD_LENGTH[0], WORD_LENGTH[1], WORD_LENGTH[2]}, WORD_LENGTH_LIMIT})
+) audio_sample_packet (
+    .frame_counter(frame_counter),
+    .valid_bit('{2'b00, 2'b00, 2'b00, 2'b00}),
+    .user_data_bit('{2'b00, 2'b00, 2'b00, 2'b00}),
+    .audio_sample_word(audio_sample_word_packet),
+    .audio_sample_word_present(audio_sample_word_present_packet),
+    .header(headers[2]),
+    .sub(subs[2])
+    );
 
 auxiliary_video_information_info_frame #(
     .VIDEO_ID_CODE(7'(VIDEO_ID_CODE)),
     .IT_CONTENT(IT_CONTENT)
-) auxiliary_video_information_info_frame(.header(headers[130]), .sub(subs[130]));
+) auxiliary_video_information_info_frame(
+    .header(headers[130]),
+    .sub(subs[130])
+    );
 
 
-source_product_description_info_frame #(.VENDOR_NAME(VENDOR_NAME), .PRODUCT_DESCRIPTION(PRODUCT_DESCRIPTION), .SOURCE_DEVICE_INFORMATION(SOURCE_DEVICE_INFORMATION)) source_product_description_info_frame(.header(headers[131]), .sub(subs[131]));
+  source_product_description_info_frame #(
+    .VENDOR_NAME(VENDOR_NAME),
+    .PRODUCT_DESCRIPTION(PRODUCT_DESCRIPTION),
+    .SOURCE_DEVICE_INFORMATION(SOURCE_DEVICE_INFORMATION)
+    ) source_product_description_info_frame(
+      .header(headers[131]),
+      .sub(subs[131])
+    );
 
-
-audio_info_frame audio_info_frame(.header(headers[132]), .sub(subs[132]));
+audio_info_frame audio_info_frame(
+  .header(headers[132]),
+   .sub(subs[132])
+  );
 
 
 // "A Source shall always transmit... [an InfoFrame] at least once per two Video Fields"
