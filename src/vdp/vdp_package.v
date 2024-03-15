6f9e20e823ab4131da811050d8a8b7d2afad04e6
Dean Netherton <dinoboards@netherton.org>
2024-03-15 14:40:12 +1100

    common-functions: removed CLOCKS_PER_LINE and added FRAME_HEIGHT

--- src/vdp/vdp_package.v
+++ src/vdp/vdp_package.v
@@ -1,21 +1,18 @@
 package custom_timings;
-  function [10:0] CLOCKS_PER_LINE;
-    input PAL_MODE;
-    begin
-      if (PAL_MODE)
-        CLOCKS_PER_LINE = 1728; // Return this value if PAL_MODE is set
-      else
-        CLOCKS_PER_LINE = 1716; // Return this value if PAL_MODE is not set
-    end
-  endfunction
-
   function [9:0] CLOCKS_PER_HALF_LINE;
     input PAL_MODE;
     begin
-      if (PAL_MODE)
-        CLOCKS_PER_HALF_LINE = 864; // Return this value if PAL_MODE is set
-      else
-        CLOCKS_PER_HALF_LINE = 858; // Return this value if PAL_MODE is not set
+      if (PAL_MODE) CLOCKS_PER_HALF_LINE = 864;  // Return this value if PAL_MODE is set
+      else CLOCKS_PER_HALF_LINE = 858;  // Return this value if PAL_MODE is not set
     end
   endfunction
+
+  function [9:0] FRAME_HEIGHT;
+    input PAL_MODE;
+    begin
+      if (PAL_MODE) FRAME_HEIGHT = 625;  // Return this value if PAL_MODE is set
+      else FRAME_HEIGHT = 525;  // Return this value if PAL_MODE is not set
+    end
+  endfunction
+
 endpackage
