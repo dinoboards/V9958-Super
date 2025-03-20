fde84e9edc4f5103acc8c1d8d546658c5ba9a3c2
Dean Netherton <dinoboards@netherton.org>
2025-03-20 17:42:38 +1100

    ENABLE_SUPER_RES: extended registers initialised on reset

--- src/vdp/vdp_register.sv
+++ src/vdp/vdp_register.sv
@@ -245,54 +245,7 @@ module VDP_REGISTER (
   bit [7:0] FF_REG_R31;
 
   bit [5:0] extended_reg_index;
-  bit [7:0] extended_super_regs[64] = '{
-      0: 8'h5B, // BUS_ARB_50HZ_START_X     Low  byte 859 (0x35B)
-      1: 8'h03, // BUS_ARB_50HZ_START_X     High byte 859 (0x35B)
-      2: 8'hD0, // BUS_ARB_50HZ_END_X       Low  byte 720 (0x2D0)
-      3: 8'h02, // BUS_ARB_50HZ_END_X       High byte 720 (0x2D0)
-
-      4: 8'h6C, // BUS_ARB_50HZ_START_Y     Low  byte 620 (0x26C)
-      5: 8'h02, // BUS_ARB_50HZ_START_Y     High byte 620 (0x26C)
-      6: 8'h40, // BUS_ARB_50HZ_END_Y       Low  byte 576 (0x240)
-      7: 8'h02, // BUS_ARB_50HZ_END_Y       High byte 576 (0x240)
-
-      8: 8'h55, // BUS_ARB_60HZ_START_X     Low  byte 853 (0x355)
-      9: 8'h03, // BUS_ARB_60HZ_START_X     High byte 853 (0x355)
-      10: 8'hD0, // BUS_ARB_60HZ_END_X      Low  byte 720 (0x2D0)
-      11: 8'h02, // BUS_ARB_60HZ_END_X      High byte 720 (0x2D0)
-
-      12: 8'h08, // BUS_ARB_60HZ_START_Y    Low  byte 520 (0x208)
-      13: 8'h02, // BUS_ARB_60HZ_START_Y    High byte 520 (0x208)
-      14: 8'hE0, // BUS_ARB_60HZ_END_Y      Low  byte 480 (0x1E0)
-      15: 8'h01, // BUS_ARB_60HZ_END_Y      High byte 480 (0x1E0)
-
-      16: 8'h5F, // VIEW_PORT_50HZ_START_X  Low  byte 864-1 (0x35F)
-      17: 8'h03, // VIEW_PORT_50HZ_START_X  High byte 864-1 (0x35F)
-      18: 8'hCF, // VIEW_PORT_50HZ_END_X    Low  byte 720-1 (0x2CF)
-      19: 8'h02, // VIEW_PORT_50HZ_END_X    High byte 720-1 (0x2CF)
-
-      20: 8'h70, // VIEW_PORT_50HZ_START_Y  Low  byte 625-1 (0x270)
-      21: 8'h02, // VIEW_PORT_50HZ_START_Y  High byte 625-1 (0x270)
-      22: 8'h3F, // VIEW_PORT_50HZ_END_Y    Low  byte 576-1 (0x23F)
-      23: 8'h02, // VIEW_PORT_50HZ_END_Y    High byte 576-1 (0x23F)
-
-      24: 8'h59, // VIEW_PORT_60HZ_START_X  Low  byte 858-1 (0x359)
-      25: 8'h03, // VIEW_PORT_60HZ_START_X  High byte 858-1 (0x359)
-      26: 8'hCF, // VIEW_PORT_60HZ_END_X    Low  byte 720-1 (0x2CF)
-      27: 8'h02, // VIEW_PORT_60HZ_END_X    High byte 720-1 (0x2CF)
-
-      28: 8'h0C, // VIEW_PORT_60HZ_START_Y  Low  byte 525-1 (0x20C)
-      29: 8'h02, // VIEW_PORT_60HZ_START_Y  High byte 525-1 (0x20C)
-      30: 8'hDF, // VIEW_PORT_60HZ_END_Y    Low  byte 480-1 (0x1DF)
-      31: 8'h01, // VIEW_PORT_60HZ_END_Y    High byte 480-1 (0x1DF)
-
-      32: 8'h68, // LOW_RES_ROW_WIDTH Low   Byte 360 (0x168)
-      33: 8'h01, // LOW_RES_ROW_WIDTH High  Byte 360 (0x168)
-      34: 8'hD0, // HIGH_RES_ROW_WIDTH Low  Byte 720 (0x2D0)
-      35: 8'h02, // HIGH_RES_ROW_WIDTH High Byte 720 (0x2D0)
-
-      default: 8'h00  // All other registers initialized to 0
-  };
+  bit [7:0] extended_super_regs[64];
 
   assign ext_reg_bus_arb_50hz_start_x = {extended_super_regs[1][1:0], extended_super_regs[0]};
   assign ext_reg_bus_arb_50hz_end_x = {extended_super_regs[3][1:0], extended_super_regs[2]};
@@ -664,6 +617,43 @@ module VDP_REGISTER (
 `ifdef ENABLE_SUPER_RES
       FF_REG_R31 <= 0;
       extended_reg_index <= 0;
+      extended_super_regs[0] <= 8'h5B; // BUS_ARB_50HZ_START_X     Low  byte 859 (0x35B)
+      extended_super_regs[1] <= 8'h03; // BUS_ARB_50HZ_START_X     High byte 859 (0x35B)
+      extended_super_regs[2] <= 8'hD0; // BUS_ARB_50HZ_END_X       Low  byte 720 (0x2D0)
+      extended_super_regs[3] <= 8'h02; // BUS_ARB_50HZ_END_X       High byte 720 (0x2D0)
+      extended_super_regs[4] <= 8'h6C; // BUS_ARB_50HZ_START_Y     Low  byte 620 (0x26C)
+      extended_super_regs[5] <= 8'h02; // BUS_ARB_50HZ_START_Y     High byte 620 (0x26C)
+      extended_super_regs[6] <= 8'h40; // BUS_ARB_50HZ_END_Y       Low  byte 576 (0x240)
+      extended_super_regs[7] <= 8'h02; // BUS_ARB_50HZ_END_Y       High byte 576 (0x240)
+      extended_super_regs[8] <= 8'h55; // BUS_ARB_60HZ_START_X     Low  byte 853 (0x355)
+      extended_super_regs[9] <= 8'h03; // BUS_ARB_60HZ_START_X     High byte 853 (0x355)
+      extended_super_regs[10] <= 8'hD0; // BUS_ARB_60HZ_END_X      Low  byte 720 (0x2D0)
+      extended_super_regs[11] <= 8'h02; // BUS_ARB_60HZ_END_X      High byte 720 (0x2D0)
+      extended_super_regs[12] <= 8'h08; // BUS_ARB_60HZ_START_Y    Low  byte 520 (0x208)
+      extended_super_regs[13] <= 8'h02; // BUS_ARB_60HZ_START_Y    High byte 520 (0x208)
+      extended_super_regs[14] <= 8'hE0; // BUS_ARB_60HZ_END_Y      Low  byte 480 (0x1E0)
+      extended_super_regs[15] <= 8'h01; // BUS_ARB_60HZ_END_Y      High byte 480 (0x1E0)
+      extended_super_regs[16] <= 8'h5F; // VIEW_PORT_50HZ_START_X  Low  byte 864-1 (0x35F)
+      extended_super_regs[17] <= 8'h03; // VIEW_PORT_50HZ_START_X  High byte 864-1 (0x35F)
+      extended_super_regs[18] <= 8'hCF; // VIEW_PORT_50HZ_END_X    Low  byte 720-1 (0x2CF)
+      extended_super_regs[19] <= 8'h02; // VIEW_PORT_50HZ_END_X    High byte 720-1 (0x2CF)
+      extended_super_regs[20] <= 8'h70; // VIEW_PORT_50HZ_START_Y  Low  byte 625-1 (0x270)
+      extended_super_regs[21] <= 8'h02; // VIEW_PORT_50HZ_START_Y  High byte 625-1 (0x270)
+      extended_super_regs[22] <= 8'h3F; // VIEW_PORT_50HZ_END_Y    Low  byte 576-1 (0x23F)
+      extended_super_regs[23] <= 8'h02; // VIEW_PORT_50HZ_END_Y    High byte 576-1 (0x23F)
+      extended_super_regs[24] <= 8'h59; // VIEW_PORT_60HZ_START_X  Low  byte 858-1 (0x359)
+      extended_super_regs[25] <= 8'h03; // VIEW_PORT_60HZ_START_X  High byte 858-1 (0x359)
+      extended_super_regs[26] <= 8'hCF; // VIEW_PORT_60HZ_END_X    Low  byte 720-1 (0x2CF)
+      extended_super_regs[27] <= 8'h02; // VIEW_PORT_60HZ_END_X    High byte 720-1 (0x2CF)
+      extended_super_regs[28] <= 8'h0C; // VIEW_PORT_60HZ_START_Y  Low  byte 525-1 (0x20C)
+      extended_super_regs[29] <= 8'h02; // VIEW_PORT_60HZ_START_Y  High byte 525-1 (0x20C)
+      extended_super_regs[30] <= 8'hDF; // VIEW_PORT_60HZ_END_Y    Low  byte 480-1 (0x1DF)
+      extended_super_regs[31] <= 8'h01; // VIEW_PORT_60HZ_END_Y    High byte 480-1 (0x1DF)
+      extended_super_regs[32] <= 8'h68; // LOW_RES_ROW_WIDTH Low   Byte 360 (0x168)
+      extended_super_regs[33] <= 8'h01; // LOW_RES_ROW_WIDTH High  Byte 360 (0x168)
+      extended_super_regs[34] <= 8'hD0; // HIGH_RES_ROW_WIDTH Low  Byte 720 (0x2D0)
+      extended_super_regs[35] <= 8'h02; // HIGH_RES_ROW_WIDTH High Byte 720 (0x2D0)
+
 `endif
     end else begin
       if ((REQ == 1'b1 && WRT == 1'b0)) begin  // READ REQUEST
