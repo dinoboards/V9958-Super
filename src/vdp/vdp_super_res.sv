885aa014fecf3c72b74992639f2f3b931b787cdc
Dean Netherton <dinoboards@netherton.org>
2025-04-01 13:09:58 +1100

    ENABLE_SUPER_RES: pipeline REG_R7_FRAME_COL to reduce latency risks

--- src/vdp/vdp_super_res.sv
+++ src/vdp/vdp_super_res.sv
@@ -90,6 +90,17 @@ module VDP_SUPER_RES (
     end
   end
 
+  bit [7:0] _REG_R7_FRAME_COL;
+
+  always_ff @(posedge reset or posedge clk) begin
+    if (reset | ~vdp_super) begin
+      _REG_R7_FRAME_COL <= 0;
+    end else begin
+      _REG_R7_FRAME_COL <= REG_R7_FRAME_COL;
+    end
+  end
+
+
   bit super_res_visible;
   bit super_res_visible_switched_on;
   bit [9:0] view_port_start_x;
@@ -135,7 +146,7 @@ module VDP_SUPER_RES (
 
 
         ext_reg_view_port_end_x: begin  //679 //cycle cx[1:0] == 3
-          PALETTE_ADDR2 <= REG_R7_FRAME_COL;
+          PALETTE_ADDR2 <= _REG_R7_FRAME_COL;
           if (super_res_visible && (super_res)) vrm_32_1 <= vrm_32;
         end
 
@@ -172,7 +183,7 @@ module VDP_SUPER_RES (
           //LOAD PALETTE_ADDR2 for first pixel of each row
           if (super_res) begin
             PALETTE_ADDR2 <= vrm_32_1[7:0];
-            first_pixel <= vrm_32_1[7:0];
+            first_pixel   <= vrm_32_1[7:0];
           end else begin
             if (!active_line || last_line) begin
               PALETTE_ADDR2 <= vrm_32_1[7:0];
@@ -181,7 +192,7 @@ module VDP_SUPER_RES (
               line_buffer[line_buffer_index] <= vrm_32_1[7:0];
             end else begin
               PALETTE_ADDR2 <= line_buffer[line_buffer_index];
-              first_pixel <= line_buffer[line_buffer_index];
+              first_pixel   <= line_buffer[line_buffer_index];
 
             end
             line_buffer_index <= 10'(line_buffer_index + 1);
@@ -191,7 +202,7 @@ module VDP_SUPER_RES (
 
         default begin
           if (!super_res_visible) begin
-            PALETTE_ADDR2 <= REG_R7_FRAME_COL;
+            PALETTE_ADDR2 <= _REG_R7_FRAME_COL;
 
           end else begin
             if (super_mid) begin
