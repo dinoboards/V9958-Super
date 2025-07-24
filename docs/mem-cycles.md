4151ca66c2b4d34a80dc5a9782d65d806c435e85
Dean Netherton <dinoboards@netherton.org>
2025-07-24 20:05:43 +1000

    superres: refactor - restructured the rendering of super res modes
    * extracted super high res and super mid res into their own individual blocks
    * removed some unneeded registers (first_pixel for super high res)

--- docs/mem-cycles.md
+++ docs/mem-cycles.md
@@ -1,6 +1,13 @@
-===================================================
+Walkthrough of state transition during video rendering from memory
 
-Start at cx == 720 && last line (call this cycle 0)
+===================================================
+## SUPER HIGH RES CYCLES
+
+Assume:
+  ext_reg_view_port_start_x = frame_width-1
+  ext_reg_view_port_end_x = 720-1
+
+Cycles start at cx == 720 && last line (call this cycle 0)
 
 Cycle: 0
   addr <= 0 (pixels: 0..3)
@@ -9,11 +16,11 @@ Cycle 4:
   addr <= 1 (pixels: 4..7)
 
 cycle 5:
-  vrm_32_1 < vrm_32  ; this is for pixels (0..3)
+  vrm_32_1 <= vrm_32  ; this is for pixels (0..3)
 
 ; lost of free cylces until line actually starts
 
-cycle 99: (cx == framewidth-1)
+cycle 99: (Not actually 99 - but after the blanking period) ext_reg_view_port_start_x - aka - (cx == framewidth-1)
   PALETTE_ADDR2 <= vrm_32_1[7:0]; Pixel 0
 
 cycle 100:
@@ -49,5 +56,107 @@ cycle 108:
   PALETTE_ADDR2 <= vrm_32_1[15:8]; Pixel 9
 
 
+## SUPER MID RES CYCLES
+
+Assume:
+  ext_reg_view_port_start_x = frame_width-1
+  ext_reg_view_port_end_x = 720-1
+
+Cycles start at cx == 720 && last line (call this cycle 0)
+
+Cycle 0:
+  addr <= 0 (bytes: 0..3)
+  line_buf_idx <= 0
+
+Cycle 4:
+  addr <= 1 (bytes: 4..7)
+
+Cycle 5:
+  mvrm_32_1 <= vrm_32 ; this is for bytes (0..3)
+
+Cycle 80: not actual 80 - but many cycles after 5 (cx == 856)
+  PALETTE_ADDR2 <= mvrm_32_1[7:0]; Pixel 0
+  first_pixel <= mvrm_32_1[7:0]; Pixel 0
+  line_buffer[0] <= mvrm_32_1[7:0]; Pixel 0
+  line_buf_idx <= line_buf_idx + 1
+  odd_phase <= 0;
+
+cycle 99: ext_reg_view_port_start_x - aka - (cx == framewidth-1)
+  PALETTE_ADDR2 <= first_pixel[7:0]; Pixel 0
 
 
+**active_line is true**
+cycle 100: State 0
+  PALETTE_ADDR2 unchanged - so still Pixel 0
+  addr <= 2 (bytes: 8..11)
+  mvrm_32_2 <= mvrm_32_1; bytes 0..3
+
+cycle 101: State 1
+  PALETTE_ADDR2 <= mvrm_32_1[15:8]; Pixel 1
+  line_buffer[1] <= mvrm_32_1[15:8]; Pixel 1
+  mvrm_32_1 <= vrm_32; this is for bytes (4..7)
+  line_buf_idx <= line_buf_idx + 1
+
+cycle 102: State 2
+  PALETTE_ADDR2 unchanged - so still Pixel 1
+
+cycle 103: State 3
+  PALETTE_ADDR2 <= mvrm_32_2[23:16]; Pixel 2
+  line_buffer[2] <= mvrm_32_2[23:16]; Pixel 2
+  line_buf_idx <= line_buf_idx + 1
+  odd_phase <= 1;
+
+cycle 104: State 4
+  PALETTE_ADDR2 unchanged - so still Pixel 2
+
+cycle 105: State 5
+  PALETTE_ADDR2 <= mvrm_32_2[31:24]; Pixel 3
+  line_buffer[3] <= mvrm_32_2[31:24]; Pixel 3
+  line_buf_idx <= line_buf_idx + 1
+
+cycle 106: State 6
+  PALETTE_ADDR2 unchanged - so still Pixel 3
+
+cycle 107: State 7
+  PALETTE_ADDR2 <= mvrm_32_1[7:0]; Pixel 4
+  line_buffer[4] <= mvrm_32_1[7:0]; Pixel 4
+  line_buf_idx <= line_buf_idx + 1
+  odd_phase <= 0;
+
+cycle 108: state 0
+  PALETTE_ADDR2 unchanged - so still Pixel 4
+  addr <= 3 (bytes: 12..15)
+  mvrm_32_2 <= mvrm_32_1; bytes 4..7
+
+cycle 109: state 1
+  PALETTE_ADDR2 <= mvrm_32_1[15:8]; Pixel 5
+  line_buffer[5] <= mvrm_32_1[15:8]; Pixel 5
+  mvrm_32_1 <= vrm_32; this is for bytes (8..11)
+  line_buf_idx <= line_buf_idx + 1
+
+cycle 110: State 2
+  PALETTE_ADDR2 unchanged - so still Pixel 5
+
+cycle 111: State 3
+  PALETTE_ADDR2 <= mvrm_32_2[23:16]; Pixel 6
+  line_buffer[6] <= mvrm_32_2[23:16]; Pixel 6
+  line_buf_idx <= line_buf_idx + 1
+  odd_phase <= 1;
+
+cycle 112: State 4
+  PALETTE_ADDR2 unchanged - so still Pixel 6
+
+cycle 105: State 5
+  PALETTE_ADDR2 <= mvrm_32_2[31:24]; Pixel 7
+  line_buffer[7] <= mvrm_32_2[31:24]; Pixel 7
+  line_buf_idx <= line_buf_idx + 1
+
+cycle 106: State 6
+  PALETTE_ADDR2 unchanged - so still Pixel 7
+
+cycle 107: State 7
+  PALETTE_ADDR2 <= mvrm_32_1[7:0]; Pixel 8
+  line_buffer[8] <= mvrm_32_1[7:0]; Pixel 8
+  line_buf_idx <= line_buf_idx + 1
+  odd_phase <= 0;
+  ...
--- src/vdp/vdp_super_res.sv
+++ src/vdp/vdp_super_res.sv
@@ -51,17 +51,9 @@ module VDP_SUPER_RES (
 
   import custom_timings::*;
 
-  bit odd_phase;
-  bit [31:0] vrm_32_2;
-  bit [31:0] vrm_32_1;
   bit super_high_res_visible_x;
   bit super_high_res_visible_y;
   bit last_line;
-  bit active_line;  // true if line is drawn from sdram, false if drawn from line buffer
-  bit [7:0] line_buffer[720];
-  bit [9:0] line_buffer_index;
-
-  assign active_line = ((super_mid) && cy[0] == 0) || super_res;
 
   bit [9:0] frame_height_minus_1;
   bit [9:0] frame_height;
@@ -145,168 +137,223 @@ module VDP_SUPER_RES (
     end
   end
 
+
+  bit [17:0] super_high_res_vram_addr;
+  bit [17:0] super_mid_res_vram_addr;
+  bit [7:0] super_high_res_palette_addr;
+  bit [7:0] super_mid_res_palette_addr;
+
+  assign super_res_vram_addr = super_res ? super_high_res_vram_addr : super_mid_res_vram_addr;
+  assign PALETTE_ADDR2 = super_res ? super_high_res_palette_addr : super_mid_res_palette_addr;
+
+
   bit [7:0] first_pixel;
+  bit active_line;  // true if line is drawn from sdram, false if drawn from line buffer
+  bit [7:0] line_buffer[720];
+  bit [9:0] line_buffer_index;
+  bit odd_phase;
+  bit [31:0] mvrm_32_1;
+  bit [31:0] mvrm_32_2;
+  assign active_line = ((super_mid) && cy[0] == 0) || super_res;
 
   always_ff @(posedge reset or posedge clk) begin
     if (reset | ~vdp_super) begin
-      super_res_vram_addr <= 0;
-      vrm_32_1 <= '{default: 0};
-      vrm_32_2 <= '{default: 0};
-      first_pixel <= '{default: 0};
-      line_buffer_index <= 0;
-      odd_phase <= 0;
+      super_mid_res_vram_addr <= 0;
+      super_mid_res_palette_addr <= '{default: 0};
 
     end else begin
       case (cx)
-        ext_reg_view_port_start_x: begin
-          if (on_a_visible_line) PALETTE_ADDR2 <= first_pixel;
-        end
-
-
-        ext_reg_view_port_end_x: begin  //679 //cycle cx[1:0] == 3
-          if (super_res_visible && (super_res)) vrm_32_1 <= vrm_32;
-        end
-
-        720: begin  //(DL)
+        720: begin
           if (last_line) begin
-            super_res_vram_addr <= ext_reg_super_res_page_addr;
+            super_mid_res_vram_addr <= ext_reg_super_res_page_addr;
           end
           line_buffer_index <= 0;
         end
 
-        //721: super_res_vram_addr will be latched into VRAM access by `ADDRESS_BUS
-
-        722: begin
+        724: begin  // cycle cx[1:0] == 0
+          if (last_line) begin
+            super_mid_res_vram_addr <= 18'(super_mid_res_vram_addr + 1);
+          end
         end
 
-        //723 VRAM refreshing
+        725: begin  //cycle cx[1:0] == 1
+          if (last_line) begin
+            mvrm_32_1 <= vrm_32;
+          end
+        end
+
+        856: begin  //cycle cx[1:0] == 2
+          //LOAD super_mid_res_palette_addr for first pixel of each row
+          if (!active_line || last_line) begin
+            super_mid_res_palette_addr <= mvrm_32_1[7:0];
+            first_pixel <= mvrm_32_1[7:0];
+
+            line_buffer[line_buffer_index] <= mvrm_32_1[7:0];
+          end else begin
+            super_mid_res_palette_addr <= line_buffer[line_buffer_index];
+            first_pixel <= line_buffer[line_buffer_index];
+
+          end
+          line_buffer_index <= 10'(line_buffer_index + 1);
+          odd_phase <= 0;
+        end
+
+        ext_reg_view_port_start_x: begin //default: frame-width -1
+          if (on_a_visible_line) super_mid_res_palette_addr <= first_pixel;
+        end
+
+        default begin
+          if (!super_res_visible) begin
+            super_mid_res_palette_addr <= _REG_R7_FRAME_COL;
+
+          end else begin
+            case ({
+              odd_phase, cx[1:0]
+            })
+              3'b000: begin
+                if (active_line) begin
+                  mvrm_32_2 <= REG_R1_DISP_ON ? mvrm_32_1 : 0;
+                  super_mid_res_vram_addr <= 18'(super_mid_res_vram_addr + 1);
+                end
+              end
+              3'b001: begin
+                if (active_line) begin
+                  super_mid_res_palette_addr <= mvrm_32_1[15:8];
+                  line_buffer[line_buffer_index] <= mvrm_32_1[15:8];
+                  mvrm_32_1 <= vrm_32;  //capture next 4 bytes
+                end else begin
+                  super_mid_res_palette_addr <= line_buffer[line_buffer_index];
+                end
+                line_buffer_index <= 10'(line_buffer_index + 1);
+              end
+              3'b010: begin
+              end
+              3'b011: begin
+                if (active_line) begin
+                  super_mid_res_palette_addr <= mvrm_32_2[23:16];
+                  line_buffer[line_buffer_index] <= mvrm_32_2[23:16];
+                end else begin
+                  super_mid_res_palette_addr <= line_buffer[line_buffer_index];
+                end
+                line_buffer_index <= 10'(line_buffer_index + 1);
+                odd_phase <= 1;
+              end
+              3'b100: begin
+              end
+              3'b101: begin
+                if (active_line) begin
+                  super_mid_res_palette_addr <= mvrm_32_2[31:24];
+                  line_buffer[line_buffer_index] <= mvrm_32_2[31:24];
+                end else begin
+                  super_mid_res_palette_addr <= line_buffer[line_buffer_index];
+                end
+                line_buffer_index <= 10'(line_buffer_index + 1);
+              end
+              3'b110: begin
+              end
+              3'b111: begin
+                if (active_line) begin
+                  super_mid_res_palette_addr <= mvrm_32_1[7:0];
+                  line_buffer[line_buffer_index] <= mvrm_32_1[7:0];
+                end else begin
+                  super_mid_res_palette_addr <= line_buffer[line_buffer_index];
+                end
+                line_buffer_index <= 10'(line_buffer_index + 1);
+                odd_phase <= 0;
+              end
+            endcase
+          end
+          end
+      endcase
+    end
+  end
+
+
+  bit [31:0] vrm_32_1;
+
+  always_ff @(posedge reset or posedge clk) begin
+    if (reset | ~vdp_super) begin
+      super_high_res_vram_addr <= 0;
+      vrm_32_1 <= '{default: 0};
+
+    end else begin
+      case (cx)
+        720: begin
+          if (last_line) begin
+            super_high_res_vram_addr <= ext_reg_super_res_page_addr;
+            //reset address to start
+            //data read at 725 into vrm_32_1
+          end
+        end
 
         724: begin  // cycle cx[1:0] == 0
           if (last_line) begin
-            super_res_vram_addr <= 18'(super_res_vram_addr + 1);
+            super_high_res_vram_addr <= 18'(super_high_res_vram_addr + 1);
+            //request 2nd set of pixels : equiv of addr <= 1
+            //read in state 3 straight into super_high_res_palette_addr
           end
         end
 
         725: begin  //cycle cx[1:0] == 1
           if (last_line) begin
             vrm_32_1 <= vrm_32;
+            // for the last line of frame (not visible)
+            //capture the data - assume this for first pixels
+            //top/left corner
+            // this is not 'apparently' impacted by addr change in 724
           end
         end
 
-        726: begin  //cycle cx[1:0] == 2
+        ext_reg_view_port_start_x: begin //default: frame-width -1
+          if (on_a_visible_line)
+            super_high_res_palette_addr <= vrm_32_1[7:0];
+            //first pixel of each row, including first row???
         end
 
-        856: begin  //cycle cx[1:0] == 2
-          //LOAD PALETTE_ADDR2 for first pixel of each row
-          if (super_res) begin
-            first_pixel   <= vrm_32_1[7:0];
-          end else begin
-            if (!active_line || last_line) begin
-              PALETTE_ADDR2 <= vrm_32_1[7:0];
-              first_pixel <= vrm_32_1[7:0];
-
-              line_buffer[line_buffer_index] <= vrm_32_1[7:0];
-            end else begin
-              PALETTE_ADDR2 <= line_buffer[line_buffer_index];
-              first_pixel   <= line_buffer[line_buffer_index];
-
-            end
-            line_buffer_index <= 10'(line_buffer_index + 1);
-            odd_phase <= 0;
-          end
+        ext_reg_view_port_end_x: begin
+          super_high_res_palette_addr <= _REG_R7_FRAME_COL;
+          vrm_32_1 <= vrm_32;  //load next 4 bytes for start of next row??
         end
 
         default begin
           if (!super_res_visible) begin
-            PALETTE_ADDR2 <= _REG_R7_FRAME_COL;
+            super_high_res_palette_addr <= _REG_R7_FRAME_COL;
+            // standard background border
 
           end else begin
-            if (super_mid) begin
-              case ({
-                odd_phase, cx[1:0]
-              })
-                3'b000: begin
-                  if (active_line) begin
-                    vrm_32_2 <= REG_R1_DISP_ON ? vrm_32_1 : 0;
-                    super_res_vram_addr <= 18'(super_res_vram_addr + 1);
-                  end
-                end
-                3'b001: begin
-                  if (active_line) begin
-                    PALETTE_ADDR2 <= vrm_32_1[15:8];
-                    line_buffer[line_buffer_index] <= vrm_32_1[15:8];
-                    vrm_32_1 <= vrm_32;  //capture next 4 bytes
-                  end else begin
-                    PALETTE_ADDR2 <= line_buffer[line_buffer_index];
-                  end
-                  line_buffer_index <= 10'(line_buffer_index + 1);
-                end
-                3'b010: begin
-                end
-                3'b011: begin
-                  if (active_line) begin
-                    PALETTE_ADDR2 <= vrm_32_2[23:16];
-                    line_buffer[line_buffer_index] <= vrm_32_2[23:16];
-                  end else begin
-                    PALETTE_ADDR2 <= line_buffer[line_buffer_index];
-                  end
-                  line_buffer_index <= 10'(line_buffer_index + 1);
-                  odd_phase <= 1;
-                end
-                3'b100: begin
-                end
-                3'b101: begin
-                  if (active_line) begin
-                    PALETTE_ADDR2 <= vrm_32_2[31:24];
-                    line_buffer[line_buffer_index] <= vrm_32_2[31:24];
-                  end else begin
-                    PALETTE_ADDR2 <= line_buffer[line_buffer_index];
-                  end
-                  line_buffer_index <= 10'(line_buffer_index + 1);
-                end
-                3'b110: begin
-                end
-                3'b111: begin
-                  if (active_line) begin
-                    PALETTE_ADDR2 <= vrm_32_1[7:0];
-                    line_buffer[line_buffer_index] <= vrm_32_1[7:0];
-                  end else begin
-                    PALETTE_ADDR2 <= line_buffer[line_buffer_index];
-                  end
-                  line_buffer_index <= 10'(line_buffer_index + 1);
-                  odd_phase <= 0;
-                end
-              endcase
-            end else if (super_res) begin
-              case (cx[1:0])
-                // During clock cycle 0:
-                //   super_res: PALETTE_ADDR2 is loaded pixel indexes [1, 5, 9, 13, ...]
-                0: begin
-                  PALETTE_ADDR2 <= vrm_32_1[15:8];
-                  super_res_vram_addr <= 18'(super_res_vram_addr + 1);
-                end
+            case (cx[1:0])
+              0: begin
+                super_high_res_vram_addr <= 18'(super_high_res_vram_addr + 1);
+                //request 12, 16, 20.. pixel sets
 
-                // During clock cycle 1:
-                //   super_res: PALETTE_ADDR2 is loaded pixel indexes [2, 6, 10, 14, ...]
-                // Request for next double word is initiated at during this clock cycle (vrm_32_1)
-                1: begin
-                  PALETTE_ADDR2 <= vrm_32_1[23:16];
-                end
+                super_high_res_palette_addr <= vrm_32_1[15:8];
+                //pixels 1, 5, 9, 13, ...
+              end
 
-                // During clock cycle 2:
-                //   super_res: PALETTE_ADDR2 is loaded pixel indexes [3, 7, 11, 15, ...]
-                2: begin
-                  PALETTE_ADDR2 <= vrm_32_1[31:24];
-                end
+              // During clock cycle 1:
+              //   super_res: super_high_res_palette_addr is loaded pixel indexes [2, 6, 10, 14, ...]
+              // Request for next double word is initiated at during this clock cycle (vrm_32_1)
+              1: begin
+                super_high_res_palette_addr <= vrm_32_1[23:16];
+              end
 
-                // During clock cycle 3:
-                //   super_res: PALETTE_ADDR2 is loaded pixel indexes [4, 8, 12, 16, ...]
-                3: begin
-                  vrm_32_1 <= vrm_32;  //load next 4 bytes
-                  PALETTE_ADDR2 <= vrm_32[7:0];
-                end
-              endcase
-            end
+              // During clock cycle 2:
+              //   super_res: super_high_res_palette_addr is loaded pixel indexes [3, 7, 11, 15, ...]
+              2: begin
+                super_high_res_palette_addr <= vrm_32_1[31:24];
+              end
+
+              // During clock cycle 3:
+              //   super_res: super_high_res_palette_addr is loaded pixel indexes [4, 8, 12, 16, ...]
+              3: begin
+                //new data request is ready (requested in state 0)
+                super_high_res_palette_addr <= vrm_32[7:0];  // for pixel 4, this is as per addr in state 724
+
+                vrm_32_1 <= vrm_32;  //load next 4 bytes
+                //on state 3, load next pixels requested at state 0
+                //this might be pixels for new row
+              end
+            endcase
           end
         end
       endcase
